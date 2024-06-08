87f9a7b8bde25868f563a226cc3792fe5f1845bd
Abhishek Sharma <asharma991@outlook.com>
2024-06-08 22:57:05 +0530

    read-header

--- app/main.js
+++ app/main.js
@@ -1,26 +1,42 @@
-const net = require("net");
+const net = require('net');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log('Logs from your program will appear here!');
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on('close', () => {
     socket.end();
     server.close();
   });
-  socket.on("data", (data) => {
+  socket.on('data', (data) => {
     console.log(data.toString());
-    const strArr = data.toString().split(" ");
+    const strArr = data.toString().split(' ');
     const target = strArr[1];
-    if (target==='/') {
+    console.log(strArr);
+    if (target === '/') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n');
       socket.end();
       return;
     }
-    if (target.includes("/echo/")) {
+    if (target.includes('/echo/')) {
       const str = target.slice(6);
       const length = str.length;
-      socket.write('HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: '+length+'\r\n\r\n'+str);
+      socket.write(
+        'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ' +
+          length +
+          '\r\n\r\n' +
+          str
+      );
+      socket.end();
+      return;
+    }
+    if (target.includes('user-agent')) {
+      const userAgentHeader = strArr[3].split('\r\n')[0];
+      socket.write(
+        'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ' +
+          userAgentHeader.length +
+          '\r\n\r\n'
+      );
       socket.end();
       return;
     }
@@ -29,5 +45,4 @@ const server = net.createServer((socket) => {
   });
 });
 
-
-server.listen(4221, "localhost");
+server.listen(4221, 'localhost');
