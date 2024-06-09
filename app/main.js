8248a7341dcb44019a9932c6328f8294f5033d5c
Abhishek Sharma <asharma991@outlook.com>
2024-06-09 13:57:12 +0530

    read body

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const fs = require('fs');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!');
@@ -12,6 +13,7 @@ const server = net.createServer((socket) => {
     console.log(data.toString());
     const strArr = data.toString().split(' ');
     const target = strArr[1];
+    const method = strArr[0];
     console.log(strArr);
     if (target === '/') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n');
@@ -42,23 +44,31 @@ const server = net.createServer((socket) => {
       return;
     }
     if (target.includes('/files/')) {
-      const file = target.slice(7);
-      const fs = require('fs');
-      try {
+      if (method === 'GET') {
+        const file = target.slice(7);
+        try {
+          const directory = process.argv[3];
+          const data = fs.readFileSync(`${directory}/${file}`).toString();
+          socket.write(
+            'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ' +
+              data.length +
+              '\r\n\r\n' +
+              data
+          );
+          socket.end();
+        } catch (e) {
+          socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+          socket.end();
+        }
+        return;
+      } else {
+        const fileName = target.slice(7);
         const directory = process.argv[3];
-        const data = fs.readFileSync(`${directory}/${file}`).toString();
-        socket.write(
-          'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ' +
-            data.length +
-            '\r\n\r\n' +
-            data
-        );
-        socket.end();
-      } catch (e) {
-        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+        const body = data.toString().split('\r\n\r\n')[1];
+        fs.writeFileSync(`${directory}/${fileName}`, body);
+        socket.write('HTTP/1.1 201 Created\r\n\r\n');
         socket.end();
       }
-      return;
     }
 
     socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
